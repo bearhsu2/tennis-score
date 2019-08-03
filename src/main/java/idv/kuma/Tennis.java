43bea91d829f1ca06081263184d8ef225968a99b
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-03 23:41:46 +0800

    refactor with map

--- src/main/java/idv/kuma/Tennis.java
+++ src/main/java/idv/kuma/Tennis.java
@@ -1,16 +1,24 @@
 package idv.kuma;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
+
+    private static Map<Integer, String> scroeToPresent = new HashMap<>();
+
+    static {
+        scroeToPresent.put(1, "Fifteen");
+        scroeToPresent.put(2, "Thirty");
+    }
 
 
     private int score1 = 0;
 
     public String score() {
 
-        if (score1 == 1) {
-            return "Fifteen Love";
-        } else if (score1 == 2) {
-            return "Thirty Love";
+        if (score1 > 0) {
+            return scroeToPresent.get(score1) + " Love";
         }
 
         return "Love All";
