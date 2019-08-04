229c7932196be664adfc1ce4f36160ead3ecd250
Yu-Song Syu <kuma@simbo.com.tw>
2019-08-04 20:30:55 +0800

    use map for score-string mapping

--- src/main/java/idv/kuma/Tennis.java
+++ src/main/java/idv/kuma/Tennis.java
@@ -1,20 +1,29 @@
 package idv.kuma;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Tennis {
 
+    private static Map<Integer, String> scoreToString = new HashMap<>();
+
+    static {
+        scoreToString.put(1, "Fifteen");
+        scoreToString.put(2, "Thirty");
+    }
 
     private int a;
 
     public String getScore() {
 
         if (a == 1) {
-            return "Fifteen Love";
+            return scoreToString.get(a) + " Love";
         }
 
         if (a == 2) {
-            return "Thirty Love";
+            return scoreToString.get(a) + " Love";
         }
-        
+
         return "Love All";
     }
 
